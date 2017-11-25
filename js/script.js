4208e300f7c4ea2d5a2fc1e72542e5ffb7610302
Kaira Villanueva <fzf.villanueva@gmail.com>
2017-11-25 13:31:15 -0500

    add

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
 	var quote = jQuery('#quote');
 	var author = jQuery('#author');
 	var URL = "https://api.myjson.com/bins/1b3wep";
-	jQuery.get(URL, function(response) { 
+	jQuery.get(URL, function(response) {
 		 var rand = Math.floor((Math.random() * 5) + 1);
 		 var rand = rand - 1;
 		 var randomQuote = response.data[ rand ].quote;
@@ -41,7 +41,7 @@ $( document ).ready(function() {
     var t_butt = $('#tutorialBlogButt');
     var r_butt = $('#researchBlogButt');
 
-    
+
     m_butt.click(function() {
         hideOrShowBlog(main,tutorial,research);
 	});
@@ -54,5 +54,31 @@ $( document ).ready(function() {
         hideOrShowBlog(research, tutorial, main);
     });
 
-});
+    $(function () {
 
+      $('.jprt-container').jportilio({
+
+      filter: [],
+
+      // the ratio of item's width to item's height
+      ratio: "0.75",
+
+      // number of items in row for small devices
+      // <768px
+      ws_xs: 1,
+
+      //  768px < x < 992px
+      ws_sm: 2,
+
+      // 992px < x < 1200px
+      ws_md: 3,
+
+      // 1200px < x
+      ws_lg: 4
+
+      });
+
+    });
+
+
+});
